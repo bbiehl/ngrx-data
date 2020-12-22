cde0769e266e64f3b5d00776f2c46de1e8b44688
Bradley Biehl <bradley.biehl@gmail.com>
2020-12-22 05:27:58 -0600

    main: added model and removed imports

--- src/app/todo-list/todo-list.module.ts
+++ src/app/todo-list/todo-list.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodoListComponent } from './todo-list.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   exports: [TodoListComponent],
